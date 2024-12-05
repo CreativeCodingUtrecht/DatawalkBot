5c27fe76476b49d29f2db948c7c4e1b05f65cd8d
Niels Janssen <niels@appliedcreative.nl>
2024-12-05 10:34:43 +0100

    Moved SvelteKit app into /app folder to enable separation of web app and data

---
+++ app/.svelte-kit/types/src/routes/(map)/datawalk/[datawalk]/proxy+page.server.ts
@@ -0,0 +1,31 @@
+// @ts-nocheck
+import type { PageServerLoad } from "./$types";
+import type { Datawalk, TrackPoint, Participant } from "$lib/database/types";
+import * as DatawalkRepository from "$lib/database/repositories/DatawalkRepository";
+import * as ParticipantRepository from "$lib/database/repositories/ParticipantRepository";
+import * as TrackPointRepository from "$lib/database/repositories/TrackPointRepository";
+import * as DataPointRepository from "$lib/database/repositories/DataPointRepository";
+
+export const load = async ({ params }: Parameters<PageServerLoad>[0]) => {
+	const code = params.datawalk as string;
+	const datawalk: any = await DatawalkRepository.findWithParticipantsByCode(code);
+
+	if (datawalk) {
+		const participants = datawalk.participants_contributing;
+		for (let participant of participants) {
+			console.log("Fetching participant's data", participant);
+			const trackpoints : any = await TrackPointRepository.find({datawalk_id: datawalk.id, participant_id: participant.id});
+
+			for (let trackpoint of trackpoints) {
+				const datapoints = await DataPointRepository.find({trackpoint_id: trackpoint.id});
+				trackpoint.datapoints = datapoints;
+			}
+			
+			participant.trackpoints = trackpoints;
+		}
+	}
+
+	return {
+		datawalk
+	};
+};
